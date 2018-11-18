cc59a3800413b746824c3700437a15283bb5b9ce
sunchenguang <sunchenguang@youzan.com>
2018-11-19 00:04:15 +0800

    add: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by suncg on 2018/11/18.
+ */
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+}
+
